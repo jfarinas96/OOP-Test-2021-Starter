8b0c870aac7c0fd9b89724ba65114cf7b9034eff
Janae Fariñas <C19708735@mytudublin.ie>
2021-04-21 20:01:11 +0100

    drawNotes method finished

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -14,6 +14,9 @@ public class ScoreDisplay extends PApplet
 	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
 	float border;
+	float h;
+	int numLines = 5;
+	float space;
 
 	public void loadScore() {
 		for (int i = 0; i < score.length(); i++) {
@@ -47,14 +50,42 @@ public class ScoreDisplay extends PApplet
 	}
 
 	public void lines() {
-		int numLines = 5;
-		float h = height * 0.2f;
-		float space = h / numLines;
+		space = h / numLines;
+
 		for (int i = 0; i < numLines; i++) {
 			stroke(0);
 			strokeWeight(3);
 
 			line(border, height * 0.4f + (space * i), width - border, height * 0.4f + (space * i));
+		}
+	}
+
+	public void drawNotes() {
+		int numNotes = notes.size();
+		float lineLength = width - border - border;
+		float lengthSpace = lineLength / numNotes;
+		float x_pos = border + (lengthSpace * 0.5f);
+
+		for (Note n:notes) {
+			float newNote = n.getNote();
+			noStroke();
+			fill(0);
+
+			if (n.getNote() < 'D') {
+				newNote = n.getNote() + 7;
+			}
+
+			float y_pos = map(newNote,'D','K', height * 0.4f + (space * 5), height * 0.4f);
+			circle(x_pos, y_pos, 22);
+
+			stroke(0);
+			line(x_pos + 11, y_pos, x_pos + 11, y_pos - (space * 2.5f));
+
+			if (n.getDuration() == 1) {
+				line(x_pos + 12, y_pos - (space * 2.5f), x_pos + 24, y_pos - (space * 2));
+			}
+
+			x_pos += lengthSpace;
 		}
 	}
 	
@@ -71,6 +102,8 @@ public class ScoreDisplay extends PApplet
 	public void setup() 
 	{
 		border = width * 0.1f;
+		h = height * 0.2f;
+
 		loadScore();
 		printScores();
 	}
@@ -80,9 +113,6 @@ public class ScoreDisplay extends PApplet
 		background(255);
 		
 		lines();
-	}
-
-	void drawNotes()
-	{
+		drawNotes();
 	}
 }
